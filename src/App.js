896a92c68a3f4486a8e7dbcbb01a62d1b2761170
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-12-17 21:40:12 -0600

    allows for changing search query from input

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import axios from 'axios'
 
 export default function App() {
   const [results, setResults] = useState([])
+  const [query, setQuery] = useState('reacthooks')
 
   useEffect(() => {
     getResults()
-  }, [])
+  }, [query])
 
   const getResults = async () => {
     const response = await axios.get(
-      'http://hn.algolia.com/api/v1/search?query=reacthooks'
+      `http://hn.algolia.com/api/v1/search?query=${query}`
     )
     setResults(response.data.hits)
   }
 
   return (
     <>
-      <h1>React Hook News</h1>
+      <h1>{query.toLocaleUpperCase()} NEWS</h1>
+      <input type="text" onChange={e => setQuery(e.target.value)} />
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
