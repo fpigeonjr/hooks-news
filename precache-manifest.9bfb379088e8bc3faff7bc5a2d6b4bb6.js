4088c2170fe315243ea38a62af80cb91dcf0dc40
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-12-19 22:35:39 -0600

    Updates

---
+++ precache-manifest.9bfb379088e8bc3faff7bc5a2d6b4bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575094c5c2a3957c4556b6153adacc96",
+    "url": "/hooks-news/index.html"
+  },
+  {
+    "revision": "a63de302f2336bb4e695",
+    "url": "/hooks-news/static/js/2.42ad47f5.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/hooks-news/static/js/2.42ad47f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61a239e36ac8747fab11",
+    "url": "/hooks-news/static/js/main.ede8edaf.chunk.js"
+  },
+  {
+    "revision": "60dd2955b61bcfc24b01",
+    "url": "/hooks-news/static/js/runtime-main.2cb8bf30.js"
+  }
+]);
